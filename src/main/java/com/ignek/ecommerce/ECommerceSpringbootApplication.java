d25943cadf86da3cd8de782cd2d8f87bfe8e66b6
vky546 <vikkeybarpete546@gmail.com>
2022-12-27 12:49:53 +0530

    project setup completed

---
+++ src/main/java/com/ignek/ecommerce/ECommerceSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ignek.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceSpringbootApplication.class, args);
+	}
+
+}
